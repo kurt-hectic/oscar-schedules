14db321f943440b5e2c9bf862365d341b628766e
Timo Proescholdt <timo@proescholdt.de>
2019-10-21 16:39:57 +0200

    do not accept zero or emmtpy intervals

--- oscar_schedules/utils.py
+++ oscar_schedules/utils.py
@@ -3,41 +3,21 @@ from oscar_schedules.schedule import Schedule
 
 def oscar2schedule(row):
 
+    month_from = int(row['MONTH_SINCE_NU'])
+    month_to = int(row['MONTH_TILL_NU'])
+    week_from = int(row['WEEKDAY_SINCE_NU']) 
+    week_to = int(row['WEEKDAY_TILL_NU'])
+    hour_from = int(row['HOUR_SINCE_NU'])
+    hour_to = int(row['HOUR_TILL_NU'])
+    min_from = int(row['MINUTE_SINCE_NU'])
+    min_to = int(row['MINUTE_TILL_NU'])
+    interval = int(row['TEMP_REP_INTERVAL_NU'])
+    if interval == 0:
+        raise ValueError("temporal reporting interval cannot be 0")
+    
+    international = row['INTERNATIONAL_EXCHANGE_YN'] == "Y"
     status = row['OPERATING_STATUS_DECLARED_WMO306']
-    try: 
-        month_from = int(row['MONTH_SINCE_NU'])
-        month_to = int(row['MONTH_TILL_NU'])
-    except ValueError :
-        month_from = 1
-        month_to = 12
-    try:
-        week_from = int(row['WEEKDAY_SINCE_NU']) 
-        week_to = int(row['WEEKDAY_TILL_NU'])
-    except ValueError :
-        week_from = 1
-        week_to = 7
-    try:
-        hour_from = int(row['HOUR_SINCE_NU'])
-        hour_to = int(row['HOUR_TILL_NU'])
-    except ValueError :
-        hour_from = 0
-        hour_to = 23
-    try:    
-        min_from = int(row['MINUTE_SINCE_NU'])
-        min_to = int(row['MINUTE_TILL_NU'])
-    except ValueError :
-        min_from = 0
-        min_to = 59
-    try:
-        interval = int(row['TEMP_REP_INTERVAL_NU'])
-        if interval == 0:
-            raise ValueError
-    except ValueError :
-        interval = 60*60*6 #6h
-
-    international = True
 
     s = Schedule(month_from,week_from,hour_from,min_from,month_to,week_to,hour_to,min_to,interval,international,status)
    
-    
     return s
