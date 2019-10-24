372176865b1d71799d0008926b33706793d7555d
Pierre Vernier <pvernier82@gmail.com>
2019-10-24 12:58:38 +0100

    Put back checks on date fields as can be empty in the schedule file

--- oscar_schedules/utils.py
+++ oscar_schedules/utils.py
@@ -3,15 +3,31 @@ from oscar_schedules.schedule import Schedule
 
 def oscar2schedule(row):
 
-    month_from = int(row["MONTH_SINCE_NU"])
-    month_to = int(row["MONTH_TILL_NU"])
-    week_from = int(row["WEEKDAY_SINCE_NU"])
-    week_to = int(row["WEEKDAY_TILL_NU"])
-    hour_from = int(row["HOUR_SINCE_NU"])
-    hour_to = int(row["HOUR_TILL_NU"])
-    min_from = int(row["MINUTE_SINCE_NU"])
-    min_to = int(row["MINUTE_TILL_NU"])
-    interval = int(row["TEMP_REP_INTERVAL_NU"])
+    try:
+        month_from = int(row['MONTH_SINCE_NU'])
+        month_to = int(row['MONTH_TILL_NU'])
+    except ValueError:
+        month_from = 1
+        month_to = 12
+    try:
+        week_from = int(row['WEEKDAY_SINCE_NU'])
+        week_to = int(row['WEEKDAY_TILL_NU'])
+    except ValueError:
+        week_from = 1
+        week_to = 7
+    try:
+        hour_from = int(row['HOUR_SINCE_NU'])
+        hour_to = int(row['HOUR_TILL_NU'])
+    except ValueError:
+        hour_from = 0
+        hour_to = 23
+    try:
+        min_from = int(row['MINUTE_SINCE_NU'])
+        min_to = int(row['MINUTE_TILL_NU'])
+    except ValueError:
+        min_from = 0
+        min_to = 59
+
     if interval == 0:
         raise ValueError("temporal reporting interval cannot be 0")
 
