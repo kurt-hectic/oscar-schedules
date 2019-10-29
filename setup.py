89c396a761c8465adc452d82cdf3f98b977f1c19
Timo Proescholdt <timo@proescholdt.de>
2019-10-29 17:24:29 +0100

    new version 0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oscar-schedules',
-    version='0.2',
+    version='0.3',
     description='library to help calculating OSCAR schedules',
     author='Timo Proescholdt',
     author_email='tproescholdt@wmo.int',
