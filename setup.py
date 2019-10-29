dbf2d9ff17fd638fcef2715fec3e0a7aa5eec483
Timo Proescholdt <timo@proescholdt.de>
2019-10-29 21:56:07 +0100

    improved to string method of schedule

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oscar-schedules',
-    version='0.3',
+    version='0.4',
     description='library to help calculating OSCAR schedules',
     author='Timo Proescholdt',
     author_email='tproescholdt@wmo.int',
