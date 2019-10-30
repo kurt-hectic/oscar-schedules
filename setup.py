453fc49509bae1f59c68972e3891a7bee136e49a
Timo Proescholdt <timo@proescholdt.de>
2019-10-30 14:59:42 +0100

    update

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import oscar_schedules
+#import oscar_schedules
 
 setup(
     name='oscar-schedules',
-    version=oscar_schedules.__version__ ,
+    #version=oscar_schedules.__version__ ,
+    version=0.64,
     description='library to help calculating OSCAR schedules',
     author='Timo Proescholdt',
     author_email='tproescholdt@wmo.int',
     url='https://github.com/kurt-hectic/oscar-schedules',
     packages=find_packages(),
     install_requires=[
-        'setuptools','requests',
+        'setuptools',
       ],
     
     )
